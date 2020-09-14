32aa9e962ed92c33080d9dba68c4973058fd2a28
Allrthur <athurbittencourt@id.uff.br>
2020-09-14 09:15:09 -0300

    exercicio 1.2
    preenchimento corrigido

--- src/main/sql/s20201/exercicios/exercicio1/ArthurBittencourt.sql
+++ src/main/sql/s20201/exercicios/exercicio1/ArthurBittencourt.sql
@@ -1,13 +1,3 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/**
- * Author:  Allrthur
- * Created: 14 de set. de 2020
- */
-
 drop table if exists pessoa cascade;
 
 create table pessoa(
@@ -15,7 +5,6 @@ name varchar,
 endereco varchar
 );
 
-insert into pessoa values ('Arthur', 'end');
-insert into endereco values ('rua dos bobos', 'end2');
+insert into pessoa values ('Arthur', 'rua dos bobos');
 
 select * from pessoa;
