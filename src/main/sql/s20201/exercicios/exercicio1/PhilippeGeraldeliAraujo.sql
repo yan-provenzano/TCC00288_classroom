f44feaac11d9776b8db6e8d65ad19e40ffb5c9b5
Philippe Geraldeli <philippe.araujo@govbr.com.br>
2020-09-14 11:19:32 -0300

    Adicionando sql da primeira aula

---
+++ src/main/sql/s20201/exercicios/exercicio1/PhilippeGeraldeliAraujo.sql
@@ -0,0 +1,12 @@
+/**
+    Author: Philippe Geraldeli Araujo
+*/
+drop table if exists pessoa  cascade;
+drop table if exists pessoa2  cascade;
+
+create table pessoa(nome varchar, endereco varchar);
+
+insert into pessoa values ('Luiz Andre','end');
+insert into pessoa values ('Luiz','end2');
+
+select * from pessoa;
