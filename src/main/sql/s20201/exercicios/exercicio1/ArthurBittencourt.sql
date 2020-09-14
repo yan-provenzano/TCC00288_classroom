a9a4aad6b9708f8de53667bc779e479ff63f5de3
Allrthur <athurbittencourt@id.uff.br>
2020-09-14 09:13:20 -0300

    exercicio 1.1
    erro no preenchimento da tabela

--- src/main/sql/s20201/exercicios/exercicio1/ArthurBittencourt.sql
+++ src/main/sql/s20201/exercicios/exercicio1/ArthurBittencourt.sql
@@ -8,3 +8,14 @@
  * Created: 14 de set. de 2020
  */
 
+drop table if exists pessoa cascade;
+
+create table pessoa(
+name varchar,
+endereco varchar
+);
+
+insert into pessoa values ('Arthur', 'end');
+insert into endereco values ('rua dos bobos', 'end2');
+
+select * from pessoa;
