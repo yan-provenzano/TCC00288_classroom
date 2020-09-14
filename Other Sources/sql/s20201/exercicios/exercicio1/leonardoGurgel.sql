4d76fe89883ef6bdc42f1b5dc8a01acea47657f8
Leonardo Gurgel <lgurgel@id.uff.br>
2020-09-14 09:04:12 -0300

    exercicio1

---
+++ Other Sources/sql/s20201/exercicios/exercicio1/leonardoGurgel.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS pessoa;
+
+CREATE TABLE pessoa(
+  nome VARCHAR,
+  endereco VARCHAR
+);
+
+INSERT INTO pessoa VALUES ('Leonardo Gurgel', 'rua aaaaaaaa');
+
+SELECT * FROM pessoa;
