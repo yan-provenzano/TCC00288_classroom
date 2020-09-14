f428cd34ac620adb2462f68f8e2fffafe3a0e9e8
GustavoSRocha <rocha_gustavo@id.uff.br>
2020-09-14 10:19:45 -0300

    Teste 1

---
+++ src/main/sql/s20201/exercicios/exercicio1/GustavoRocha.sql
@@ -0,0 +1,21 @@
+#######
+
+drop table if exists pessoa  cascade;
+drop table if exists pessoa2  cascade;
+
+create table pessoa(
+nome varchar,
+endereco varchar
+);
+
+create table pessoa2(
+nome varchar,
+endereco varchar
+);
+
+insert into pessoa values ('Gustavo Rocha','end');
+insert into pessoa values ('Gustavo','end2');
+
+
+
+select * from pessoa;
