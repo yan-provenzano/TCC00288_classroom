95f178378ef9239c1c1d8bd8ca6d68fcb7fc2574
Joao pedro de almeida <almeidajoao@id.uff.br>
2020-09-14 09:20:06 -0300

    Create JoaoPedroDeAlmeidaOliveiraCosta.sql
    Fazendo primeiro commit

---
+++ src/main/sql/s20201/exercicios/exercicio1/JoaoPedroDeAlmeidaOliveiraCosta.sql
@@ -0,0 +1,21 @@
+drop table if exists pessoa  cascade;
+drop table if exists pessoa2  cascade;
+
+create table pessoa(
+nome varchar,
+endereco varchar
+);
+
+create table pessoa2(
+nome varchar,
+endereco varchar
+);
+
+insert into pessoa values ('Luiz Andre','end');
+insert into pessoa values ('Luiz','end2');
+insert into pessoa2 values ('Luiz Andre','end');
+insert into pessoa2 values ('Luiz','end2');
+
+
+select * from pessoa;
+select * from pessoa2;
