ffc90d26dd3a4671bd5a111d653966633b0add7e
AmeliaDWho <71261272+AmeliaDWho@users.noreply.github.com>
2020-09-14 09:12:13 -0300

    colocado tabela pessoa2 no arquivo

---
+++ src/main/sql/s20201/exercicios/exercicio1/Amelia.sql
@@ -0,0 +1,10 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  Gabri
+ * Created: 14 de set. de 2020
+ */
+
