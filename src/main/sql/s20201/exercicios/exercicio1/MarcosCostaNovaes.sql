c1e9226d4855144ece0c01ab51a02f20019cf3f4
Marcos Novaes <marcosnovaes@id.uff.br>
2020-09-16 08:55:01 -0300

    exercicio 1 marcos da costa novaes

---
+++ src/main/sql/s20201/exercicios/exercicio1/MarcosCostaNovaes.sql
@@ -0,0 +1,14 @@
+create table pessoa
+(
+    nome varchar,
+    endereco varchar
+);
+insert into pessoa
+values
+    ('Marcos Novaes', 'Rua das mariolas');
+insert into pessoa
+values
+    ('Carlos Eduardo', 'rua dos bobos');
+
+select *
+from pessoa;
